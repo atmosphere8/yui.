67f8a11867afb74cdbcd81aee67912f0df5047bc
atmosphere8 <azurrodneyyy@gmail.com>
2025-08-16 12:13:19 +0200

    add eas build configuration #13, resize logo.png #14

--- app.config.ts
+++ app.config.ts
@@ -19,10 +19,20 @@ const config: ExpoConfig = {
     supportsTablet: true,
   },
   android: {
+    package: "com.atmosphere8.yui",
     edgeToEdgeEnabled: true,
+    adaptiveIcon: {
+      foregroundImage: `${icons}/logo.png`,
+      backgroundColor: "#fff",
+    },
   },
   web: {
     favicon: `${icons}/logo.png`,
+  },
+  extra: {
+    eas: {
+      projectId: "e48036ad-be4c-4ebe-9951-137f8321b969",
+    },
   },
   plugins: ["expo-router"],
 };
